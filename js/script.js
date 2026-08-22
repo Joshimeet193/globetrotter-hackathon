e0876de78f7d73a3a96b07e0905db6311b192809
Joshimeet193 <joshimeet193@gmail.com>
2026-08-22 11:20:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+// GlobeTrotter - small UI polish, no framework needed.
 
+document.addEventListener('DOMContentLoaded', function () {
+  var navbar = document.querySelector('.navbar');
+
+  // Shrink navbar + stronger shadow after scrolling a bit
+  if (navbar) {
+    var onScroll = function () {
+      if (window.scrollY > 20) {
+        navbar.classList.add('navbar-scrolled');
+      } else {
+        navbar.classList.remove('navbar-scrolled');
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+    onScroll();
+  }
+
+  // Auto-close the mobile menu after tapping a link (Bootstrap doesn't do this by default)
+  var navCollapse = document.querySelector('.navbar-collapse');
+  if (navCollapse) {
+    navCollapse.querySelectorAll('a.nav-link, a.navbar-profile-link').forEach(function (link) {
+      link.addEventListener('click', function () {
+        if (navCollapse.classList.contains('show') && window.bootstrap) {
+          var instance = window.bootstrap.Collapse.getInstance(navCollapse) ||
+                          new window.bootstrap.Collapse(navCollapse);
+          instance.hide();
+        }
+      });
+    });
+  }
+});
